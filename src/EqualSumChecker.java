990d63d299575c006970cc2afa721d0840a9763e
Victor-Aremu <77357316+Victor-Aremu@users.noreply.github.com>
2021-01-17 16:26:46 +0000

    small changes made

--- src/EqualSumChecker.java
+++ src/EqualSumChecker.java
@@ -1,19 +1,11 @@
 public class EqualSumChecker {
-    public static void main(String[] args) {
-
-        System.out.println(hasEqualSum(1,1,1));
-        System.out.println(hasEqualSum(1,1,2));
-        System.out.println(hasEqualSum(1,-1,0));
-
-    }
-    public static boolean hasEqualSum (int number1, int number2, int number3){
-
-        if (number1 + number2 == number3){
+    public static boolean hasEqualSum(int number1, int number2, int number3){
+        if ((number1 + number2) == number3){
             return true;
-        }else {
-
+        }
+        else {
             return false;
         }
     }
 }
-// Victor-Aremu.
+//Victor.Aremu
